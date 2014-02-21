e1abdcaa0c0d8a47334bb9d6d94408415a7d223f
brianwhelan <kardkontrol@gmail.com>
2014-02-21 14:44:57 +0000

    First Commit

---
+++ Display/Display/CreateList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Display
+{
+    class CreateList
+    {
+        // I want to read in a text file and 
+        // put it into a list
+
+        public List<string> WordList = new List<string>();
+
+        public CreateList(string FileLocation) : this()
+        {
+
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\WORDS.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                WordList.Add(line);
+            }
+
+            file.Close();
+
+
+        }
+
+        public CreateList()
+        {
+            //Nothing Happens
+        }
+
+    }
+}
---
+++ Display/Display/Game1.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Display
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont x;
+
+        Texture2D texture;
+
+        List<string> Words = new List<string>();
+        CreateList test = new CreateList("C:\\WORDS.txt");
+        int totheleft = 10;
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            x = Content.Load<SpriteFont>("MyFont");
+
+            texture = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            texture.SetData<Color>(new Color[] { Color.White });
+            
+
+
+            Words = test.WordList;
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            //spriteBatch.Draw(texture, new Rectangle(100, 100, 10, 10), Color.White);
+
+            for (int i = 0; i < Words.Count; i++)
+            {
+              
+                spriteBatch.DrawString(x, Words[i], new Vector2(10 + totheleft, 10 + (30 * i)), Color.White,
+                    (float)0.0, new Vector2(), (float)(Words.Count - i) * 0.25f, SpriteEffects.None, (float)1.0);
+                
+            }
+
+            totheleft++;
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
